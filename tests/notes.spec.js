dab9d76a7bc3d1c01244bd35592781205392f323
Gabriel Lundgren <glundgren93@gmail.com>
2017-01-17 23:17:33 -0200

    adding tests

---
+++ tests/notes.spec.js
@@ -0,0 +1,24 @@
+const notes = require("../notes");
+
+/**
+ * Clear Note files before each test
+ */
+beforeEach(() => {
+  notes.clearNotes();
+});
+
+describe("note", () => {
+  it("should add a note", () => {
+    expect(notes.addNote("NoteTitle", "NoteBody")).toBeTruthy();
+  });
+
+  it("should delete an added note", () => {
+    notes.addNote("NoteTitle", "NoteBody");
+    expect(notes.deleteNote("NoteTitle")).toBeTruthy();
+  });
+
+  it("should read an added note", () => {
+    notes.addNote("NoteTitle", "NoteBody");
+    expect(notes.getNote("NoteBody")).not.toBeNull();
+  });
+});
